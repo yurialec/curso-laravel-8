b661bd2aac664527410a280bab47d229bd429160
Yuri Alec <yurialec@hotmail.com>
2022-11-15 16:28:38 -0300

    create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,14 @@
 <h1>Novo Post</h1>
+@if($errors->any())
+    <ul>
+        @foreach($errors->all() as $erro)
+            <li>{{$erro}}</li>
+        @endforeach
+    </ul>
+@endif
 <form action="{{ route('posts.store') }}" method="post">
     @csrf
-    <input type="text" name="title" id="title" placeholder="Título">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
+    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo" value="{{ old('content') }}"></textarea>
     <button type="submit">Enviar</button>
 </form>

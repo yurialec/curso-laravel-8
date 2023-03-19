cd0d110447ec638e846fa01acf69303c337d1d00
yuri <yurialec@hotmail.com>
2023-03-19 18:27:06 -0300

    Melhorias forms

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,18 +1,6 @@
 <h1>Editar o Post {{ $post->title }}</h1>
-
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
-
 <form action="{{ route('posts.update', $post->id) }}" method="post">
-    @csrf
     @method('put')
-    <input type="text" name="title" id="title" placeholder="Título" value="{{ $post->title }}">
-    <textarea name="content" id="content" cols="30" rows="4">{{ $post->content }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.posts._partials.form')
 </form>
 <a href="{{ route('posts.index') }}">início</a>
